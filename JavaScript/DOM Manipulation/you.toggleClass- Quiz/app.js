d3db984c9d37405f8fb12f87e223fcd02eef7ca5
Sravanthi <sravanthi.cb@gmail.com>
2018-01-23 13:37:18 -0500

    Create app.js

---
+++ JavaScript/DOM Manipulation/you.toggleClass- Quiz/app.js
@@ -0,0 +1,10 @@
+/*
+For this quiz, use a jQuery class selector and featuredArticle variable to toggle the 'featured' class!
+*/
+
+// don't change this variable!
+var featuredArticle;
+
+featuredArticle = $("div.articles").toggleClass("article-item featured");
+// your code starts here!
+console.log(featuredArticle);
