dc64ee2a26b3bbb3464db874be9f169512500e9c
Sravanthi <sravanthi.cb@gmail.com>
2018-01-23 13:49:06 -0500

    Create app.js

---
+++ JavaScript/DOM Manipulation/4 Quiz Changing Attributes Quiz/app.js
@@ -0,0 +1,10 @@
+/*
+For this quiz, set the href of the <a> in the first nav item to "#1".
+
+You must use jQuery's attr() method!
+*/
+
+// Start with this variable!
+var navList;
+
+navList = $( 'ul li a:first-child' ).attr( "href", "#1" );
