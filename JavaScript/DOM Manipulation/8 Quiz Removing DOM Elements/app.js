4fc981db5fdfd90602f6abde32dab0c9b37441ff
Sravanthi <sravanthi.cb@gmail.com>
2018-01-23 13:53:57 -0500

    Create app.js

---
+++ JavaScript/DOM Manipulation/8 Quiz Removing DOM Elements/app.js
@@ -0,0 +1,14 @@
+/*
+For this quiz, remove the <ul> from the first article item!
+
+You must use jQuery's remove() method.
+*/
+
+// Start with this variable!
+var articleItems, ul;
+
+articleItems = $(".article-item").first();
+
+ul = articleItems.children("ul");
+
+ul.remove();
